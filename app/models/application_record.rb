b3dda73a6ab643d9cfa21faf0999b4ca8f7ad072
Vikas Singh <vikassingh37111@gmail.com>
2019-05-30 14:58:32 +0530

    get history added

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,13 +3,17 @@ class ApplicationRecord < ActiveRecord::Base
 
 	after_commit :store_history
 	
+	def get_history
+		ModelHistory.where(:record_id => id, :model => model_name.to_s).all
+	end
+
 	def store_history
 		ModelHistory.create(
 			record_id: id,
 			model: model_name.to_s,
 			serialized_obj: attributes.to_json,
 			action_time: Time.now,
-			actor_id: (defined? current_user) ? current_user : 0,
+			actor_id: (defined? current_user) ? current_user : 0,  # Zero for backend job
 			action: created_at == updated_at ? "create" : "update"
 		)
 	end
